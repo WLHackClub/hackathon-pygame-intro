5a2402727da9e62f2956515bb6cabceee6d2384d
greatericontop <greateric@protonmail.com>
2024-09-25 09:42:26 -0400

    add this

--- new1.py
+++ new1.py
@@ -7,15 +7,10 @@ canvas = pygame.display.set_mode((1200, 800))
 
 def main():
     my_font = pygame.font.SysFont('Calibri', 20)
-    rendered_text = my_font.render('This is some text', True, (0, 0, 255))
-    canvas.blit(rendered_text, (200, 200))
-
-    pygame.draw.rect(canvas, (255, 255, 255), (300, 300, 50, 50))
-
-    points = [(400, 400), (400, 500), (500, 500), (500, 400), (450, 330), (400, 400)]
-    pygame.draw.aalines(canvas, (0, 255, 0), True, points)
+    clock = pygame.time.Clock()
 
     while True:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -24,6 +19,15 @@ def main():
                 canvas.fill((0, 0, 0))
                 rendered_text = my_font.render('You clicked at '+str(event.pos), True, (0, 255, 0))
                 canvas.blit(rendered_text, (300, 300))
+
+        rendered_text = my_font.render('This is some text', True, (0, 0, 255))
+        canvas.blit(rendered_text, (200, 200))
+
+        pygame.draw.rect(canvas, (255, 255, 255), (300, 300, 50, 50))
+
+        points = [(400, 400), (400, 500), (500, 500), (500, 400), (450, 330), (400, 400)]
+        pygame.draw.aalines(canvas, (0, 255, 0), True, points)
+
         pygame.display.update()
 
 
